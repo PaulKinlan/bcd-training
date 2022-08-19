591eea0e848381ce0d0426489c25a91f5483c6f7
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-19 23:08:00 +0100

    extension instead

--- main.ts
+++ main.ts
@@ -17,11 +17,11 @@ class StaticFileHandler {
 
   handler(request: Request): Response {
     const pathname = new URL(request.url).pathname;
-    const filename = pathname.substr(pathname.lastIndexOf("/")+1);
+    const extension = pathname.substr(pathname.lastIndexOf("."));
     const resolvedPathname = (pathname == "" || pathname == "/") ? "/index.html" : pathname;
     const path = join(Deno.cwd(), this.#basePath, resolvedPathname)
     const file = Deno.readFile(path)
-      .then(data => new Response(data, {status: 200, headers: { 'content-type': contentType(filename)}})) // Need to think about content tyoes.
+      .then(data => new Response(data, {status: 200, headers: { 'content-type': contentType(extension)}})) // Need to think about content tyoes.
       .catch(_ => new Response("Not found", { status: 404 }));
 
     return file;
