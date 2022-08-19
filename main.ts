39aa55b1b116ba0868ed29b189259185e0ce8f74
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-19 19:47:58 +0100

    Serving the response async (remove await)

--- main.ts
+++ main.ts
@@ -2,39 +2,33 @@ import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 
 
 class StripStream extends TransformStream {
+  constructor() {
 
+    let parsedFirstChunk: boolean = false;
+    super({
+      transform(chunk, controller) {
+        if (parsedFirstChunk == false) {
+          // 41,  93, 125,  39, 10 == ")]}'\n"
+          chunk[0] = 0;
+          chunk[1] = 0;
+          chunk[2] = 0;
+          chunk[3] = 0;
 
-
-    constructor() {
-
-        let parsedFirstChunk: boolean = false;
-        console.log("constuctor")
-        super({
-            transform(chunk, controller) {
-                if (parsedFirstChunk == false) {
-                    // 41,  93, 125,  39, 10 == ")]}'\n"
-                    chunk[0] = 0;
-                    chunk[1] = 0;
-                    chunk[2] = 0;
-                    chunk[3] = 0;
-
-                    controller.enqueue(chunk);
-                    parsedFirstChunk = true;
-                }
-                else {
-                    controller.enqueue(chunk);
-                }
-
-
-            }
-        })
-    }
+          controller.enqueue(chunk);
+          parsedFirstChunk = true;
+        }
+        else {
+          controller.enqueue(chunk);
+        }
+      }
+    })
+  }
 }
 
-serve(async (req: Request) => {
-    const version = new URL(req.url).searchParams.get("version") || 100;
+serve((req: Request) => {
+  const version = new URL(req.url).searchParams.get("version") || 100;
 
-    const response = await fetch(`https://chromestatus.com/api/v0/features?milestone=${version}`);
+  const featuresResponse = fetch(`https://chromestatus.com/api/v0/features?milestone=${version}`);
 
-    return new Response(response.body.pipeThrough(new StripStream()));
+  return new Response(featuresResponse.then(response => response.body.pipeThrough(new StripStream())));
 });
