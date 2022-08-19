c4a0e0459f836872f2c9e69b35b7af8b457a76a9
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-19 23:21:22 +0100

    Adding lit and some templating

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,10 +1,26 @@
+import {html, render} from 'https://unpkg.com/lit-html?module';
+
 onload = () => {
   const versionEl = document.getElementById("version");
+  const versions = [ ...Array(104).keys() ];
+
+  render(html`${versions.map((item) => html`<option value=${item}>${item}</option>`)}`, versionEl);
 
   versionEl.onchange = async (e) => {
     const version = e.target.value;
     const versionResponse = await fetch(`/api/features?version=${version}`);
     const versionData = await versionResponse.json();
     console.log(versionData)
+
+    const enabled = versionData["Enabled by default"]
+    const removed = versionData["Removed"]
+
+    const output = `
+    <h2>Enabled by default</h2>
+    ${enabled.forEach(item=>'hello')}
+    `
+
+    render(myTemplate('World'), document.body);
+
   };
 };
