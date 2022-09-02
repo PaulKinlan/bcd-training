3f6fb8321003484d94ddd961ef61b23f10d01685
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-02 22:17:26 +0100

    formatting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,7 +10,7 @@ class Browsers {
     this.#browsers = browsers;
   }
 
-  getBrowserReleaseDate = (browser, version) : Set => {
+  getBrowserReleaseDate = (browser, version): Set => {
     const releases = {};
 
     return this.#browsers[browser].releases[version].release_date;
@@ -18,18 +18,18 @@ class Browsers {
 }
 
 const renderBrowsers = (browsers, selectedBrowsers: Set) => {
-  return template`${ Object.entries(browsers).map(([browser, details]) => template`<input type=checkbox name="${browser}" id="${browser}" checked="${selectedBrowsers.has(browser)}">
-  <label for="${browser}">${details.name}</label>` ) }`
+  return template`${Object.entries(browsers).map(([browser, details]) => template`<input type=checkbox name="${browser}" id="${browser}" checked="${selectedBrowsers.has(browser)}">
+  <label for="${browser}">${details.name}</label>`)}`
 };
 
 const parseSelectedBrowsers = (request: Request) => {
   const url = new URL(request.url);
-  return new Set(url.searchParams.keys()); 
+  return new Set(url.searchParams.keys());
 };
 
-export default function render(request: Request, bcd) : Response {
+export default function render(request: Request, bcd): Response {
 
-  const {__meta, browsers} = bcd;
+  const { __meta, browsers } = bcd;
 
   const selectedBrowsers = parseSelectedBrowsers(request);
 
@@ -56,5 +56,5 @@ export default function render(request: Request, bcd) : Response {
     <footer><p>Using BCD version: ${__meta.version}, generated on ${__meta.timestamp}</p></footer>
 	</body>
   </html>`
-		.then(data => new Response(data, { status: 200, headers: { 'content-type': 'text/html' } }));
+    .then(data => new Response(data, { status: 200, headers: { 'content-type': 'text/html' } }));
 };
