80b5ac100625a5f1e832075adcd2d58d67131905
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-02 22:52:04 +0100

    fixing td

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -146,7 +146,7 @@ export default function render(request: Request, bcd): Response {
       <tbody>
         ${
           features.map(feature=> template`<tr><td>${feature[1]}</td>
-          <td>${feature[2]}/td>
+          <td>${feature[2]}</td>
           <td>${feature[3]}</td>
           <td>${feature[4]}</td>
           <td>${feature[5]}</td></tr>`)
