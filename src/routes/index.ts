181f72db3272e4bf831641cbf7521a5b18302c6d
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-02 21:32:04 +0100

    testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,34 @@
 import template from "../flora.ts";
 import data from "https://esm.sh/@mdn/browser-compat-data@latest/data.json" assert { type: "json" };
 
+class Browsers {
+  #browsers;
+  constructor(browsers) {
+    delete browsers.opera;
+    delete browsers.node;
+    delete browsers.webview_android;
+
+    this.#browsers = browsers;
+  }
+
+  getBrowserReleaseDate = (browser, version) => {
+    const releases = {};
+
+    return this.#browsers[browser].releases[version].release_date;
+  };
+}
+
+delete data.webextensions;
+delete data.webdriver;
+delete data.svg;
+delete data.mathml;
+
+
 export default function render(request: Request) : Response {
+
+  d
+
+
   return template`<html>
 
   <head>
