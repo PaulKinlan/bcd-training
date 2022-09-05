9243ceb126aaf20826684014bc647b7d3c589c2d
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-05 11:00:08 +0100

    ADding in configurable features

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -16,16 +16,26 @@ class Browsers {
 }
 
 const renderBrowsers = (browsers, selectedBrowsers: Set) => {
-  return template`${Object.entries(browsers).map(([browser, details]) => template`<input type=checkbox name="${browser}" id="${browser}" ${selectedBrowsers.has(browser) ? template`checked=checked` : template``}>
-  <label for="${browser}">${details.name}</label>`)}`
+  return template`${Object.entries(browsers).map(([browser, details]) => template`<input type=checkbox name="browser-${browser}" id="browser-${browser}" ${selectedBrowsers.has(browser) ? template`checked=checked` : template``}>
+  <label for="browser-${browser}">${details.name}</label>`)}`
 };
 
-
+const renderFeatures = (features, selectedFeatures: Set) => {
+  return template`${Object.entries(features).map(([feature, details]) => template`<input type=checkbox name="feature-${feature}" id="feature-${feature}" ${selectedFeatures.has(feature) ? template`checked=checked` : template``}>
+  <label for="feature-${feature}">${details.name}</label>`)}`
+};
 
 const parseSelectedBrowsers = (request: Request) => {
   const url = new URL(request.url);
-  return new Set(url.searchParams.keys());
+
+  return new Set([...url.searchParams.keys()].filter(key => key.startsWith('browser-')).map(key => key.replace('browser-', ''))));
 };
+
+const parseSelectedFeatures = (request: Request) => {
+  const url = new URL(request.url);
+  return new Set([...url.searchParams.keys()].filter(key => key.startsWith('feature-')).map(key => key.replace('feature-', ''))));
+};
+
 
 function* itterateFeatures(parent, data) {
   for (let [topLevelAPI, information] of Object.entries(data)) {
@@ -41,7 +51,7 @@ function* itterateFeatures(parent, data) {
   }
 }
 
-const getStableFeatures = (browsers, mustBeIn: Set, data ) => {
+const getStableFeatures = (browsers, mustBeIn: Set, data) => {
   const output = [];
   for (let [api, compat] of itterateFeatures("", data)) {
     if ("__compat" in compat) {
@@ -50,7 +60,7 @@ const getStableFeatures = (browsers, mustBeIn: Set, data ) => {
       let isStable = false;
       for (let [browser, support] of Object.entries(compat.__compat.support)) {
         if (mustBeIn.has(browser) == false) continue; // skip if we are not looking for this browser
-        
+
         if ("version_added" in support === false && Array.isArray(support)) {
           support = support[0] // Smash in the first answer for now.
         }
@@ -77,8 +87,8 @@ const getStableFeatures = (browsers, mustBeIn: Set, data ) => {
       }
 
       if (isStable == false) continue; // Not stable, skip.
-      
-      if(dates.length == 0) continue; // we cant work out if its in a a brower due to weird data.
+
+      if (dates.length == 0) continue; // we cant work out if its in a a brower due to weird data.
 
       // Order the data so we can pick out the first and last.
       dates.sort(function (a, b) {
@@ -105,11 +115,13 @@ const getStableFeatures = (browsers, mustBeIn: Set, data ) => {
 
 export default function render(request: Request, bcd): Response {
 
-  const { __meta, browsers, api, css, html, javascript} = bcd;
+  const { __meta, browsers, api, css, html, javascript } = bcd;
+  const features = { 'api': { name: "DOM API" }, 'css': { name: "CSS" }, 'html': { name: "HTML" }, 'javascript': { name: "JavaScript" } };
 
   const helper = new Browsers(browsers);
 
   const selectedBrowsers = parseSelectedBrowsers(request);
+  const selectedFeatures = parseSelectedFeatures(request);
 
   const features = getStableFeatures(browsers, selectedBrowsers, { api, css, html, javascript });
 
@@ -141,10 +153,7 @@ export default function render(request: Request, bcd): Response {
       </fieldset>
       <fieldset>
         <legend>Features</legend>
-        <input type=checkbox name=html id=html value=html> <label for=javascript>HTML</label>
-        <input type=checkbox name=javascript id=javascript value=javascript> <label for=javascript>JavaScript</label>
-        <input type=checkbox name=api id=api value=api> <label for=api>DOM API</label>
-        <input type=checkbox name=css id=css value=css> <label for=api>CSS</label>
+        ${renderFeatures(features, selectedFeatures)}
       </fieldset>
       <input type=reset>
       <input type=submit>
@@ -166,11 +175,10 @@ export default function render(request: Request, bcd): Response {
         <td>Days</td>
       </thead>
       <tbody>
-        ${
-          features.map(feature=> template`<tr>
+        ${features.map(feature => template`<tr>
           <td>${feature[1]}</td><td>${feature[3]}</td><td>${feature[2]}</td>
           <td>${feature[5]}</td><td>${feature[4]}</td><td>${feature[7]}</td></tr>`)
-        }
+    }
       </tbody>
     </table>
     <footer><p>Using BCD version: ${__meta.version}, generated on ${__meta.timestamp}</p></footer>
