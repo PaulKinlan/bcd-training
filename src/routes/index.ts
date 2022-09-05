0b6e97cea9adeb77847c7efc554f5e6b62409453
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-05 15:17:03 +0100

    escaping

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -223,7 +223,7 @@ export default function render(request: Request, bcd): Response {
     </table>
 
     <h3>Raw Data</h3>
-    ${[...selectedFeatures].map(feature => template`<a href="#${feature}-table>${feature}</a> `)}
+    ${[...selectedFeatures].map(feature => template`<a href="#${feature}-table>"${feature}</a> `)}
     ${stableFeatures.map(feature => {
     let response;
     if (currentCategory != feature.category) {
