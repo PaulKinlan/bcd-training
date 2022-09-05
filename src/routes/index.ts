760e53123cae1436195ff3aba7228b4fb684cc3d
Paul Kinlan <paul.kinlan@gmail.com>
2022-09-05 18:45:16 +0100

    Fixing the same issue as previous commit on index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -245,12 +245,11 @@ export default function render(request: Request, bcd): Response {
           </thead>
           <tbody>`
     }
-    else {
-      response = template`<tr>
-          <td><a href="${feature.mdn_url}">${feature.api}</a></td><td>${helper.getBrowserName(feature.firstBrowser)}</td><td>${feature.firstDate.toLocaleDateString()}</td>
-          <td>${helper.getBrowserName(feature.lastBrowser)}</td><td>${feature.lastDate.toLocaleDateString()}</td><td>${feature.ageInDays}</td></tr>`
-    }
-
+    
+    response = template`<tr>
+      <td><a href="${feature.mdn_url}">${feature.api}</a></td><td>${helper.getBrowserName(feature.firstBrowser)}</td><td>${feature.firstDate.toLocaleDateString()}</td>
+      <td>${helper.getBrowserName(feature.lastBrowser)}</td><td>${feature.lastDate.toLocaleDateString()}</td><td>${feature.ageInDays}</td></tr>`
+    
     currentCategory = feature.category;
 
     return response;
