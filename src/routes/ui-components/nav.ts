1e8e15f7922d60ee06ec9e78c775b33340d60d03
Paul Kinlan <paul.kinlan@gmail.com>
2023-02-10 10:10:44 +0000

    Adding new menu items

--- src/routes/ui-components/nav.ts
+++ src/routes/ui-components/nav.ts
@@ -7,6 +7,10 @@ export default function renderBrowsers(): ReadableStream<any> {
       <li><a href="/">Time to Stable</a></li>
       <li><a href="/not-stable">Not Yet Stable</a></li>
       <li><a href="/when-stable">Now Stable</a></li>
+      <li><a href="/experimental">Experimental APIs</a></li>
+      <li><a href="/deprecated">Deprecated APIs</a></li>
+      <li><a href="/removed">APIs that are no longer on the web</a></li>
+      <li><a href="/all">All APIs (for export)</a></li>
   </ol>
 </nav>`;
 }
